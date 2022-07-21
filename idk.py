647cc084fc1f4fb89221df2fae3604f1df52ffe9
Soggywaffles27 <99052580+Soggywaffles27@users.noreply.github.com>
2022-07-21 09:34:26 -0400

    Update idk.py

--- idk.py
+++ idk.py
@@ -1,1 +1,8 @@
 pip install tk
+from tkinter import *
+    master = Tk()
+    def close_window():
+        print("hhhhhhh")
+    button = Button(master, text = 'Click me', command = close_window)
+    button.pack()
+    mainloop()

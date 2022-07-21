c7732fabc9740aa35e09ef5ceaffaa68abb607ed
Soggywaffles27 <99052580+Soggywaffles27@users.noreply.github.com>
2022-07-21 10:37:51 -0400

    Delete idk.py

--- idk.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-def d():
-    master = Tk()
-    def close_window():
-        print("hhhhhhh")
-    button = Button(master, text = 'Click me', command = close_window)
-    button.pack()
-    mainloop()
